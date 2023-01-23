d05fd1eb8ae485988f7901a5310874705796ccf6
Andre Melo <programador.sly@gmail.com>
2023-01-23 09:28:06 -0300

    Possible moves of a piece

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -18,4 +18,11 @@ public class Rook extends ChessPiece {
             return "♜";
         }
     }
+
+    @Override
+    public boolean[][] possibleMoves() {
+        boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+        return mat;
+    }
 }
